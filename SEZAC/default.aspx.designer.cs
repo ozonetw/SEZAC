11172bd39e6de6a8381fca11ef1b36dbb85b6097
Ozone <ozonetw@gmail.com>
2015-06-07 15:49:42 -0500

    Version Inicial

---
+++ SEZAC/default.aspx.designer.cs
@@ -0,0 +1,15 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace SEZAC {
+    
+    
+    public partial class _default {
+    }
+}
---
+++ SEZAC/login.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="SEZAC.login" MasterPageFile="~/Sezac.Master"%>
+<asp:Content ContentPlaceHolderID="ContentPlaceHolder1" ID="loginform" runat="server">    
+    <center>
+    <asp:Login ID="Login1" runat="server"></asp:Login>
+    </center>
+   
+</asp:Content>
