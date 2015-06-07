89d362fdacf5d0c290a9e1bf103bdc307407f065
nancyBeltran <nancy8beltran@gmail.com>
2015-06-07 18:07:58 -0500

    holiiiiiiiiiiiiiiiiiiiiiii

---
+++ Control/Datos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control
+{
+    class Datos
+    {
+        public bool Validar(string usuario, string contraseña)
+        {
+            string consulta = "SELECT * FROM sezac.usuario where Usuario = '"+usuario+"' AND Contrasenia = '"+contraseña+"'";
+            return true;
+        }
+
+        public bool CrearDependencias(string nombre)
+        {
+            string insertar = "INSERT INTO `sezac`.`dependencia` (`Nombre`) VALUES('"+nombre+"')";
+            return true;
+        }
+        public bool AltaBeneficiarios(string[] datos, byte[] foto)
+        {
+            string insertar = "INSERT INTO `sezac`.`beneficiario` (`RFC`, `Nombres`, `ApellidoPaterno`, `ApellidoMaterno`, `Correo`, `Imagen`, `TipoUsuarioId`, `EstatusBeneficiarioId`) VALUES('"+datos[0]+"', '"+datos[1]+"', '"+datos[2]+"', '"+datos[3]+"', '"+datos [4]+"', "+foto+", '3', '1')";
+            return true;
+        }
+        public bool AltaAñoFiscal(int año)
+        {
+            string insertarAño= "INSERT INTO `sezac`.`aniofiscal` (`Anio`) VALUES('"+año+"')";
+            return true;
+        }
+    }
+}
