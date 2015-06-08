1bfdd3a752fadf9813bd173977c45133d3184b8a
rmelchorv <rmelchorv@gmail.com>
2015-06-08 00:22:25 -0500

    v

---
+++ SEZAC/Default.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Sezac.IU.Default" MasterPageFile="~/Sezac.Master" %>
+
+<asp:Content ContentPlaceHolderID="ContentPlaceHolder1" ID="loginform" runat="server">
+</asp:Content>
---
+++ SEZAC/Default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using Sezac.Control.Comun;
+using Sezac.Control.Entidades;
+using System;
+using System.Web.Security;
+using System.Web.UI;
+
+namespace Sezac.IU
+{
+	public partial class Default : Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			if (!IsPostBack)
+			{
+				if (!Request.IsAuthenticated)
+					Response.Redirect(FormsAuthentication.LoginUrl, true);
+
+				Usuario usuario = (Usuario)Session["Usuario"];
+
+				switch (usuario.Tipo)
+				{
+					case Definiciones.TipoUsuario.Administrador:
+						Response.Redirect("Admin/Default.aspx", true);
+						break;
+					case Definiciones.TipoUsuario.Encargado:
+						break;
+					case Definiciones.TipoUsuario.Beneficiario:
+						break;
+					default:
+						break;
+				}
+			}
+		}
+	}
+}
