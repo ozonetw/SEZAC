041c079055ab33f84fa3b41db1e9bed684113f47
rmelchorv <rmelchorv@gmail.com>
2015-06-07 19:23:50 -0500

    V

--- Control/Datos.cs
+++ Control/Datos.cs
@@ -10,10 +10,11 @@ namespace Sezac.Control
     {
         public bool Validar(string usuario, string contrasenia)
         {
-            Planificador planificador = new Planificador();
             Cifrado cifrado = new Cifrado(Definiciones.TipoCifrado.AES);
             Conexion conexion = new Conexion()
             {
+                #region Inicializar 
+
                 BaseDatos = "sezac",
                 Credenciales = new Credenciales()
                 {
@@ -23,32 +24,51 @@ namespace Sezac.Control
                 },
                 Servidor = "localhost",
                 Tipo = Definiciones.TipoConexion.CredencialesExplicitas,
+
+                #endregion
             };
+            Planificador planificador = new Planificador();
             Sentencia sentencia = new Sentencia()
             {
+                #region Inicializar
+
                 TextoComando = "SELECT * FROM sezac.usuario where Usuario = '" + usuario + "' AND Contrasenia = '" + contrasenia + "'",
                 Tipo = Definiciones.TipoSentencia.Query,
                 TipoComando = CommandType.Text,
                 TipoManejadorTransaccion = Definiciones.TipoManejadorTransaccion.NoTransaccion,
                 TipoResultado = Definiciones.TipoResultado.Conjunto
+
+                #endregion
             };
-            DataTable resultado = (DataTable)planificador.Servir(conexion, new List<Sentencia>() {sentencia});
+            DataTable resultado = (DataTable)planificador.Servir(
+                #region Ejecutar
+
+                conexion, new List<Sentencia>() 
+                { 
+                    sentencia
+                }
+
+                #endregion
+            );
+
             return true;
         }
 
-        public bool CrearDependencias(string nombre)
+        public bool CrearDependencia(string nombre)
         {
             string insertar = "INSERT INTO `sezac`.`dependencia` (`Nombre`) VALUES('"+nombre+"')";
             return true;
         }
-        public bool AltaBeneficiarios(string[] datos, byte[] foto)
+
+        public bool AltaBeneficiario(string[] datos, byte[] foto)
         {
-            string insertar = "INSERT INTO `sezac`.`beneficiario` (`RFC`, `Nombres`, `ApellidoPaterno`, `ApellidoMaterno`, `Correo`, `Imagen`, `TipoUsuarioId`, `EstatusBeneficiarioId`) VALUES('"+datos[0]+"', '"+datos[1]+"', '"+datos[2]+"', '"+datos[3]+"', '"+datos [4]+"', "+foto+", '3', '1')";
+            string insertar = "INSERT INTO `sezac`.`beneficiario` (`RFC`, `Nombres`, `ApellidoPaterno`, `ApellidoMaterno`, `Correo`, `Imagen`, `TipoUsuarioId`, `EstatusBeneficiarioId`) VALUES('" + datos[0] + "', '" + datos[1] + "', '" + datos[2] + "', '" + datos[3] + "', '" + datos [4] + "', " + foto + ", '3', '1')";
             return true;
         }
-        public bool AltaAñoFiscal(int año)
+
+        public bool AltaAnioFiscal(int anio)
         {
-            string insertarAño= "INSERT INTO `sezac`.`aniofiscal` (`Anio`) VALUES('"+año+"')";
+            string insertar = "INSERT INTO `sezac`.`aniofiscal` (`Anio`) VALUES('" + anio + "')";
             return true;
         }
     }
