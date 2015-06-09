b4c9b281ceff6d7cdf783b05439cdcd9f6a8fd4c
rmelchorv <rmelchorv@gmail.com>
2015-06-09 11:24:35 -0500

    vProgramasDone!

---
+++ Control/Entidades/Programa.cs
@@ -0,0 +1,30 @@
+﻿namespace Sezac.Control.Entidades
+{
+    public class Programa
+    {
+        #region Propiedades
+
+		public AnioFiscal AnioFiscal
+		{
+			get;
+			set;
+		}
+		public Dependencia Dependencia
+		{
+			get;
+			set;
+		}
+        public int Id
+        {
+            get;
+            set;
+        }
+        public string Descripcion
+        {
+            get;
+            set;
+        }
+
+        #endregion
+    }
+}
---
+++ Control/Programa.cs
@@ -0,0 +1,152 @@
+﻿using Sezac.Persistencia.Comun;
+using Sezac.Persistencia.Entidades;
+using Sezac.Persistencia.Reglas;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Sezac.Control
+{
+    public class Programa
+    {
+        #region Atributos
+
+        Conexion _conexion;
+        Scheduler _planificador;
+
+        #endregion
+
+        #region Constructor
+
+        public Programa()
+        {
+            _conexion = new Conexion()
+            {
+                #region Inicializar
+
+                Nombre = "SEZAC",
+                Tipo = Definiciones.TipoConexion.NombreConexion,
+                TipoCliente = Definiciones.TipoCliente.MySql
+
+                #endregion
+            };
+            _planificador = new Scheduler();
+        }
+
+        #endregion
+
+        #region Metodos
+
+		public bool ExistePrograma(string nombrePrograma)
+        {
+			Sentencia sentencia = new Sentencia()
+            {
+                #region Inicializar
+
+				Comando = "SELECT * FROM sezac.programa WHERE UPPER(nombre)='" + nombrePrograma.ToUpper() + "'",
+                Tipo = Definiciones.TipoSentencia.Query,
+                TipoComando = CommandType.Text,
+                TipoTransaccion = Definiciones.TipoTransaccion.NoTransaccion,
+                TipoResultado = Definiciones.TipoResultado.Entero
+
+                #endregion
+            };
+			DataTable resultado = (DataTable)_planificador.Despachar(
+                #region Ejecutar
+
+                _conexion, new List<Sentencia>() 
+                { 
+                    sentencia
+                }
+
+                #endregion
+            );
+
+            return resultado.Rows.Count > 0;
+        }
+
+		public bool InsertarPrograma(Entidades.Programa programa)
+		{
+			Sentencia sentencia = new Sentencia()
+            {
+                #region Inicializar
+
+				Comando = "INSERT INTO sezac.programa (nombre,dependenciaid,aniofiscal) VALUES ('" + programa.Descripcion + "',"+ programa.Dependencia.Id + "," + programa.AnioFiscal.Anio + ")",
+				Tipo = Definiciones.TipoSentencia.NoQuery,
+                TipoComando = CommandType.Text,
+                TipoTransaccion = Definiciones.TipoTransaccion.NoTransaccion,
+                TipoResultado = Definiciones.TipoResultado.Entero
+
+                #endregion
+            };
+
+			_planificador.Despachar(
+                #region Inicializar
+
+                _conexion, new List<Sentencia>() 
+                { 
+                    sentencia
+                }
+
+                #endregion
+            );
+            return true;
+		}
+
+        public List<Entidades.Programa> ObtenerPrograma(int id)
+        {
+            List<Entidades.Programa> programas = new List<Entidades.Programa>();
+            Sentencia sentencia = new Sentencia()
+            {
+                #region Inicializar
+
+                Comando = "SELECT * FROM sezac.programa WHERE id=COALESCE(" + ((id == 0) ? "NULL" : id.ToString()) + ",id)",
+                Tipo = Definiciones.TipoSentencia.Query,
+                TipoComando = CommandType.Text,
+                TipoTransaccion = Definiciones.TipoTransaccion.NoTransaccion,
+                TipoResultado = Definiciones.TipoResultado.Conjunto
+
+                #endregion
+            };
+            DataTable resultado = (DataTable)_planificador.Despachar(
+                #region Inicializar
+
+                _conexion, new List<Sentencia>() 
+                { 
+                    sentencia
+                }
+
+                #endregion
+            );
+
+            #region Recuperar registros
+
+            for (int indice = 0; indice < resultado.Rows.Count; indice++)
+            {
+                #region Establecer valores
+
+                programas.Add(new Entidades.Programa()
+                    {
+						Id = int.Parse(resultado.Rows[indice]["Id"].ToString()),
+						Descripcion = resultado.Rows[indice]["Nombre"].ToString(),
+						Dependencia = new Entidades.Dependencia()
+						{
+							Id = int.Parse(resultado.Rows[indice]["DependenciaId"].ToString()),
+							Descripcion = resultado.Rows[indice]["Dependencia"].ToString()
+						},
+						AnioFiscal = new Entidades.AnioFiscal()
+						{
+							Anio = int.Parse(resultado.Rows[indice]["AnioFiscal"].ToString())
+						}
+                    }
+                );
+
+                #endregion
+            }
+
+            #endregion
+            return programas;
+        }
+        
+		#endregion
+    }
+}
